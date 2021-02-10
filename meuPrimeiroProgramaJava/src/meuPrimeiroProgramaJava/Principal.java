d435d022a96a7668cad050d1ab2a38d930646dd7
Eduardo Akira <70441378+EduardoAkira18@users.noreply.github.com>
2021-02-10 20:07:03 -0300

    Update Principal.java
    Adicionado um comentário

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -2,9 +2,9 @@
  * Meu primeiro programa Java
  * 
  * Este programa mostra ao aluno:
- * 1) Que um programa Java È construÌdo dentro de um projeto Java.
- * 2) Todo programa Java deve ter uma classe com um mÈtodo main.
- * 3) Como exibir informaÁıes no terminal (console) do Java.
+ * 1) Que um programa Java √© constru√≠do dentro de um projeto Java.
+ * 2) Todo programa Java deve ter uma classe com um m√©todo main.
+ * 3) Como exibir informa√ß√µes no terminal (console) do Java.
  * 4) Como ler uma string do terminal (console) do Java.
  */
 package meuPrimeiroProgramaJava;
@@ -14,18 +14,18 @@ import java.util.Scanner;
 /**
  * @author Akira Data: 03/02/2021
  */
-public class Principal { // Classe que contÈm o mÈtodo principal.
+public class Principal { // Classe que cont√©m o m√©todo principal.
 	public static void main(String[] args) {
 		
-		
-		System.out.println("Ol· amigo!\nQual È o seu nome?");
+		// Salda√ß√£o e pergunta o nome.
+		System.out.println("Ol√° amigo!\nQual √© o seu nome?");
 
 		// Ler uma string do console usando a biblioteca Scanner.
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 
 		// Exibir um string usando printf.
-		System.out.printf("Ol· %s!", nome);
+		System.out.printf("Ol√° %s!", nome);
 		in.close();
 	}
 
